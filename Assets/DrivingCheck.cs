03165a31ee2f69a94873371bbd2b6677c1f401ae
Pomichal <vajk.pomichal@gmail.com>
2021-01-14 19:21:20 +0100

    ADD: DrivingCheck solution

--- Assets/DrivingCheck.cs
+++ Assets/DrivingCheck.cs
@@ -24,6 +24,8 @@ public class DrivingCheck : MonoBehaviour
         int age = int.Parse(ageInput.text);
 
         // TODO: print out, whether the person may drive a car (in Slovakia)
+        bool result = withInstructor || (hasLicence && age >= 18) || (hasLicence && withResponsivePerson && age >=17);
+        PrintResult(result.ToString());
     }
 
     public void PrintResult(string result)
